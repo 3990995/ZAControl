13c50710f0bcde99f88707c2979ceef503da63d3
yourgame@163.com <yourgame@163.com>
2014-07-11 11:10:24 +0800

    234234
    234234
    
    Signed-off-by: yourgame@163.com <yourgame@163.com>

--- ZAControl/ZATextFieldView.m
+++ ZAControl/ZATextFieldView.m
@@ -10,22 +10,10 @@
 
 @implementation ZATextFieldView
 
-- (id)initWithFrame:(CGRect)frame
-{
+
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
     return self;
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
