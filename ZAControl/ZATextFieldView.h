6535f6a9efae0c84100cda40193802b3c09cc36c
yourgame@163.com <yourgame@163.com>
2014-07-11 11:10:16 +0800

    323423
    4234324
    
    Signed-off-by: yourgame@163.com <yourgame@163.com>

---
+++ ZAControl/ZATextFieldView.h
@@ -0,0 +1,13 @@
+//
+//  ZATextFieldView.h
+//  ZAControl
+//
+//  Created by liao on 14-7-11.
+//  Copyright (c) 2014年 Zhenai Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZATextFieldView : UIView
+
+@end
---
+++ ZAControl/ZATextFieldView.m
@@ -0,0 +1,31 @@
+//
+//  ZATextFieldView.m
+//  ZAControl
+//
+//  Created by liao on 14-7-11.
+//  Copyright (c) 2014年 Zhenai Inc. All rights reserved.
+//
+
+#import "ZATextFieldView.h"
+
+@implementation ZATextFieldView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
